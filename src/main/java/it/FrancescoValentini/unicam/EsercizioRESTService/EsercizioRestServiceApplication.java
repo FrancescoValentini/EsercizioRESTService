78d78c8ab647ee2c8aaef619e28a9d53cf881c95
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-01-09 20:04:38 +0100

    Inizializzazione repository

---
+++ src/main/java/it/FrancescoValentini/unicam/EsercizioRESTService/EsercizioRestServiceApplication.java
@@ -0,0 +1,13 @@
+package it.FrancescoValentini.unicam.EsercizioRESTService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioRestServiceApplication.class, args);
+	}
+
+}
