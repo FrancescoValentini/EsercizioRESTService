982ae54a12a2c3df6a80ce58eb80c3212a4791cb
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-01-10 12:16:57 +0100

    Aggiunto il filtro nella securityFilterChain

--- src/main/java/it/FrancescoValentini/unicam/EsercizioRESTService/Config/SecurityConfig.java
+++ src/main/java/it/FrancescoValentini/unicam/EsercizioRESTService/Config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package it.FrancescoValentini.unicam.EsercizioRESTService.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import it.FrancescoValentini.unicam.EsercizioRESTService.Filters.JWTFilter;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Autowired
+	JWTFilter jwtFilter;
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(c -> c.disable()); // consente csrf
@@ -15,6 +22,9 @@ public class SecurityConfig {
 		http.sessionManagement(
 				session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER)
 				); // Spegne le policy di creazione delle sessioni
+		
+		// filtro per autenticazione
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		// Autorizza tutte le richieste senza autenticazione (solo per TEST) 
 		http.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
