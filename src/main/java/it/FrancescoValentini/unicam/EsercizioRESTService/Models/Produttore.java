4adc7eed3190fa78e5a5b4e0c302c0ee8681ca7d
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-01-10 10:07:42 +0100

    Inseriti username e password in Produttore

--- src/main/java/it/FrancescoValentini/unicam/EsercizioRESTService/Models/Produttore.java
+++ src/main/java/it/FrancescoValentini/unicam/EsercizioRESTService/Models/Produttore.java
@@ -2,6 +2,8 @@ package it.FrancescoValentini.unicam.EsercizioRESTService.Models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -9,9 +11,13 @@ import jakarta.persistence.OneToMany;
 @Entity
 public class Produttore {
 	@Id
-	String id;
-	String nome;
-	String cognome;
+	private String id;
+	private String nome;
+	private String cognome;
+	private String username;
+	
+	@JsonIgnore
+	private String password;
 
 	/*
 	 * Relazione Uno a molti
@@ -23,10 +29,20 @@ public class Produttore {
 	public Produttore() {
 	}
 
-	public Produttore(String id, String nome, String cognome) {
+	public Produttore(String id, String nome, String cognome, String password) {
 		this.id = id;
 		this.nome = nome;
 		this.cognome = cognome;
+		this.username = nome + "." + cognome;
+		this.password = password;
+	}
+	
+	public Produttore(String id, String nome, String cognome,String username ,String password) {
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
 	}
 
 	public String getNome() {
@@ -48,4 +64,34 @@ public class Produttore {
 	public String getId() {
 		return id;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
